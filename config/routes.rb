878e0020f5d98183c29f2808386bf74842fddb22
Neal Sutaria <nealsutaria99@gmail.com>
2018-05-19 10:31:49 -0400

    added a background pic

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :cocktails do
       resources :doses , only: [ :new, :create]
     end
+    root to: "cocktails#index"
     resources :doses, only: [:destroy]
 end
 
